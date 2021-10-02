4cd3cada6854206de033c79edc17306fbb53fe50
HLyczak <91788712+HLyczak@users.noreply.github.com>
2021-10-02 16:07:18 +0200

    update

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pierwszy_1
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja emerytura");
+            Console.Write("Podaj swoje imię: ");
+            string imie = Console.ReadLine();
+            
+           
+            
+            Console.WriteLine($"Hello {imie}");
+        }
+    }
+}
+
